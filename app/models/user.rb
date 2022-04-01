dd44bd3d127a9c6619a2716cbf8b6eedb6e7d22f
kawabata <s1931115@s.konan-u.ac.jp>
2022-04-01 15:48:15 +0900

    secure password set user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # 右式はselfを省略できる
   before_save { email.downcase! }
+  has_secure_password
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :name, presence: true, length: { maximum: 50 }
@@ -8,4 +9,5 @@ class User < ApplicationRecord
             format: { with: VALID_EMAIL_REGEX },
             # INFO Mysqlではデフォルトで大文字小文字の区別はないが、他のデータベースを使用する可能性もあるので考慮する
             uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
 end
