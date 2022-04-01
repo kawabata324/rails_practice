f0dc66e184ebd129844b3c39d9ba9c3e0e35bfed
kawabata <s1931115@s.konan-u.ac.jp>
2022-04-01 15:08:36 +0900

    add email unique and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  # 右式はselfを省略できる
+  before_save { email.downcase! }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-  validates :name, presence: true, length: {maximum: 50}
-  validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            # INFO Mysqlではデフォルトで大文字小文字の区別はないが、他のデータベースを使用する可能性もあるので考慮する
+            uniqueness: { case_sensitive: false }
 end
